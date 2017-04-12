c73a14711caa18737eaf9aaca6c294b27cd171b4
Hanbo Wang <hanbo.wang@gmail.com>
2017-04-12 12:39:36 +0800

    fix

--- make.bat
+++ make.bat
@@ -1,2 +1,2 @@
 call mvn package
-copy /Y target\result-export-2.0-jar-with-dependencies.jar target\ResultExport.jar
+copy /Y target\result-merge-1.0-jar-with-dependencies.jar target\ResultMerge.jar
